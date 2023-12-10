57bbc4a2ca0c4ff57330e8f3dac68c33cd809eae
GIRISH G <91957405+Girishg17@users.noreply.github.com>
2023-12-10 12:21:26 +0530

    Updated Solution - GfG to GitHub

--- Easy/Transpose of Matrix/transpose-of-matrix.cpp
+++ Easy/Transpose of Matrix/transpose-of-matrix.cpp
@@ -10,8 +10,10 @@ class Solution
     void transpose(vector<vector<int> >& matrix, int n)
     { 
         // code here 
-        for(int i=0;i<n ;i++){
-            for(int j=0;j<=i;j++){
+        int row=matrix.size();
+        int col=matrix[0].size();
+        for(int i=0;i<row;i++){
+            for(int j=0;j<i;j++){
                 int temp=matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=temp;
